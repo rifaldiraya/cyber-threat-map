e05de0cf84f8cd65e6de4441488ab86170a4fa25
Rifaldi Raya <rifaldi.rayaa@gmail.com>
2024-02-07 09:51:07 +0700

    fix: dot and line design

--- src/components/AttackPoint.tsx
+++ src/components/AttackPoint.tsx
@@ -11,17 +11,20 @@ export default function AttackPoint({ from, to }: TAttackPoint) {
   return (
     <>
       <Marker coordinates={from}>
-        <circle r={4} fill="green" />
+        <circle r={1} fill="green" />
       </Marker>
 
       <Marker coordinates={to}>
-        <circle r={4} fill="red" />
+        <circle r={1} fill="red" />
       </Marker>
+      {/* <svg className="dash" height="210" width="300" xmlns="http://www.w3.org/2000/svg">
+        <line x1="14.006" y1="30.7128" x2="379" y2="380" style={{ stroke: "red", strokeWidth: 1 }} />
+      </svg> */}
       <Line
         className="dash"
         onClick={(e) => console.log(e)}
         // fill="transparent"
-        style={{ color: "blue", borderColor: "blue" }}
+        style={{ color: "blue", borderColor: "blue", strokeWidth: 1 }}
         from={from}
         to={to}
       />
--- src/utils/dummy.ts
+++ src/utils/dummy.ts
@@ -8,29 +8,29 @@ export const dummyCoordinate = [
   },
   {
     coordinate: {
-      from: [-74.006, 40.7128],
+      from: [14.006, 30.7128],
       to: [379, 380],
     },
     attackName: "DDOS",
   },
   {
     coordinate: {
-      from: [-74.006, 50.7128],
-      to: [379, 380],
+      from: [14.006, 30.7128],
+      to: [260, 380],
     },
     attackName: "DDOS",
   },
   {
     coordinate: {
-      from: [-84.006, 40.7128],
-      to: [279, 390],
+      from: [14.006, 30.7128],
+      to: [280, 400],
     },
     attackName: "DOS",
   },
   {
     coordinate: {
-      from: [-74.006, 10.7128],
-      to: [379, 380],
+      from: [14.006, 30.7128],
+      to: [450, 385],
     },
     attackName: "DOS",
   },
